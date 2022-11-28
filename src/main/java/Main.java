b350b8dc34e7fca533d71cd9111c7fac0f1018ce
bszybiak <szybaikbartlomiej@gmail.com>
2022-11-28 18:26:02 +0100

    Task sort string by Integer

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,5 +4,6 @@ public class Main {
 
 //        tasks.order("Thi1s is4 T3est 2a");
     tasks.order("1df g5fd g2dg f3d !@#$ fgfd4 fsc6d adbfh7dscf af8dg.,.");
+    tasks.orderInteger("1rewr 2rgfd 3fdsdgf dfdgdf6");
     }
 }
--- src/main/java/Tasks.java
+++ src/main/java/Tasks.java
@@ -4,14 +4,17 @@ import java.util.stream.IntStream;
 
 public class Tasks {
     public String order(String words) {
-
         String s = Arrays.stream(words.replaceAll("[^a-z]", "").split(""))
                 .sorted(Comparator.comparing(String::toString)).distinct().reduce(String::concat).get();
-
         System.out.println(s);
+        return s;
 
-
-        return null;
-
+    }
+    public String orderInteger(String words){
+        String s = Arrays.stream(words.split(" "))
+                .sorted(Comparator.comparing(e -> Integer.valueOf(e.replaceAll("[^0-9]", ""))))
+                .reduce((a, b) -> a + " " + b).get();
+        System.out.println(s);
+        return s;
     }
 }
