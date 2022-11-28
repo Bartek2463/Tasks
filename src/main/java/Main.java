f9aba16fd5e0985d4fdf143cb5a027542eb011bd
bszybiak <szybaikbartlomiej@gmail.com>
2022-11-28 18:20:05 +0100

    Task filter string and sort natural order

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         Tasks tasks = new Tasks();
 
-        tasks.order("Thi1s is2 T4est 3a");
+//        tasks.order("Thi1s is4 T3est 2a");
+    tasks.order("1df g5fd g2dg f3d !@#$ fgfd4 fsc6d adbfh7dscf af8dg.,.");
     }
 }
--- src/main/java/Tasks.java
+++ src/main/java/Tasks.java
@@ -5,10 +5,13 @@ import java.util.stream.IntStream;
 public class Tasks {
     public String order(String words) {
 
-        String s1 = Arrays.stream(words.split(" "))
-                .sorted(Comparator.comparing(s -> Integer.valueOf(s.replaceAll("[^0-9]",""))))
-                .reduce((a, b) -> a + " " + b).get();
-        System.out.println(s1);
-        return s1;
+        String s = Arrays.stream(words.replaceAll("[^a-z]", "").split(""))
+                .sorted(Comparator.comparing(String::toString)).distinct().reduce(String::concat).get();
+
+        System.out.println(s);
+
+
+        return null;
+
     }
 }
