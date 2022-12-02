7c99968a53ea4cc7a8e8200eb0d33739b17bf883
bszybiak <szybaikbartlomiej@gmail.com>
2022-12-02 22:09:58 +0100

    Task sort string by Integer

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,7 +3,8 @@ public class Main {
         Tasks tasks = new Tasks();
 
 //        tasks.order("Thi1s is4 T3est 2a");
-    tasks.order("1df g5fd g2dg f3d !@#$ fgfd4 fsc6d adbfh7dscf af8dg.,.");
-    tasks.orderInteger("1rewr 2rgfd 3fdsdgf dfdgdf6");
+//    tasks.order("1df g5fd g2dg f3d !@#$ fgfd4 fsc6d adbfh7dscf af8dg.,.");
+//    tasks.orderInteger("4rewr 1rgfd 2fdsdgf dfd7g df6 kljh5");
+        tasks.orderInteger("wer-wer-trg-ytr-ytr");
     }
 }
--- src/main/java/Tasks.java
+++ src/main/java/Tasks.java
@@ -1,20 +1,23 @@
 import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class Tasks {
     public String order(String words) {
-        String s = Arrays.stream(words.replaceAll("[^a-z]", "").split(""))
+        return Arrays.stream(words.replaceAll("[^a-z]", "").split(""))
                 .sorted(Comparator.comparing(String::toString)).distinct().reduce(String::concat).get();
-        System.out.println(s);
-        return s;
 
     }
     public String orderInteger(String words){
-        String s = Arrays.stream(words.split(" "))
-                .sorted(Comparator.comparing(e -> Integer.valueOf(e.replaceAll("[^0-9]", ""))))
-                .reduce((a, b) -> a + " " + b).get();
-        System.out.println(s);
-        return s;
+        List<String> collect = Arrays.stream(words.split(""))
+                .sorted(Comparator.comparing(String::toString))
+                .collect(Collectors.toList());
+
+
+        System.out.println(collect);
+
+        return null;
     }
+
 }
